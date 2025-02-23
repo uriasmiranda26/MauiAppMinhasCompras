87efecec99322405550c3de12f12be3fb007c1a2
uriasmiranda26 <uriasmiranda41@outlook.com>
2025-02-23 18:58:29 -0300

    Construção da classe para fazer CRUD no SQLite
    Refatora estrutura do aplicativo e integração com SQLite
    
    - Atualiza `App.xaml.cs` para usar `NavigationPage` com `ListaProduto`.
    - Modifica `SQLiteDatabaseHelper` para incluir métodos de CRUD assíncronos e torna a classe pública.
    - Ajusta `Produto` para incluir atributos de chave primária e auto-incremento, melhorando a estrutura do modelo de dados.

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,15 +1,20 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Views;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaProduto());
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
+       /* protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
-        }
+        }*/
     }
 }
