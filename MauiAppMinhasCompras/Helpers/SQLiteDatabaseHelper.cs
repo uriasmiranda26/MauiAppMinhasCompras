fec1473fb66faccb8e87621b48cd8126e0e066f0
uriasmiranda26 <uriasmiranda41@outlook.com>
2025-03-08 22:08:17 -0300

    Adiciona gerenciamento de banco de dados SQLite e novas páginas
    Implementa a classe SQLiteDatabaseHelper para gerenciar a conexão e operações com o banco de dados SQLite. Atualiza a classe App para inicializar a página principal como ListaProduto.
    
    Modifica a interface da ListaProduto, alterando seu título e adicionando itens de barra de ferramentas. Implementa navegação para a página NovoProduto e adiciona campos de entrada para descrição, quantidade e preço unitário.
    
    Adiciona funcionalidade para salvar novos produtos no banco de dados e exibir mensagens de alerta em caso de erro.

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,10 +1,12 @@
 ﻿using MauiAppMinhasCompras.Models;
 using SQLite;
+
 namespace MauiAppMinhasCompras.Helpers
 {
     public class SQLiteDatabaseHelper
     {
         readonly SQLiteAsyncConnection _conn;
+
         public SQLiteDatabaseHelper(string path)
         {
             _conn = new SQLiteAsyncConnection(path);
