475f8e03d5c71a913f3d8008a6c5a687fbf8cdb9
uriasmiranda26 <uriasmiranda41@outlook.com>
2025-03-16 13:38:32 -0300

    Corrige consultas SQL e melhora gerenciamento de produtos
    - Corrigida consulta SQL no `SQLiteDatabaseHelper.cs`.
    - Adicionadas propriedades privadas e validações em `Produto.cs`.
    - Atualizado `EditarProduto.xaml` para permitir edição de campos.
    - Implementado método de atualização no `EditarProduto.xaml.cs`.
    - Melhorada interface em `ListaProduto.xaml` com barra de pesquisa.
    - Utilizada `ObservableCollection` em `ListaProduto.xaml.cs` para gerenciar produtos.
    - Adicionados métodos para busca, soma total e remoção de produtos.
    - Implementada navegação para edição de produtos selecionados.

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,10 +4,53 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+        double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                /*
+                if (value <= 0)
+                {
+                    throw new Exception("Por favor, preencha a quantidade");
+                }
+                */
+                _quantidade = value;
+            }
+        }
+        public double Preco { 
+            get=> _preco;
+            set
+            {
+                /*
+                if (value <= 0)
+                {
+                    throw new Exception("Por favor, preencha o campo preco");
+                }
+                */
+                _preco = value;
+
+            }
+        }
+        public double Total { get => Quantidade * Preco; }
     }
 }
